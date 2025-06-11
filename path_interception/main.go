36a82f419415af1d8bd7a96aab7da3136e56698e
svintus1 <ilya.semenuaka@gmail.com>
2025-06-11 23:59:42 +0300

    Доработан path_interception

--- path_interception/main.go
+++ path_interception/main.go
@@ -10,41 +10,54 @@ import (
 )
 
 var (
-	fileName = "myapp"
-	newDir   = "/tmp/bin"
-	fakeCmd  = "su"
+	fileName    = "myapp"
+	tempDirPath = "/tmp/bin"
+	fakeCmd     = "su"
 )
 
 func main() {
+	filePath := filepath.Join("assets", fileName)
+
 	if !system.IsRoot() {
 		fmt.Printf("%sThe script must be run using root permissions\n", tags.Err)
 		return
 	}
+
 	fmt.Printf("%sT1574.007 Path Interception by PATH Environment Variable\n", tags.Info)
-	err := os.Mkdir(newDir, 0755)
+
+	createDir(tempDirPath)
+	defer deleteDir(tempDirPath)
+
+	fakeCmdPath := filepath.Join(tempDirPath, fakeCmd)
+	system.UpdatePath([]string{tempDirPath})
+
+	err := system.CopyFile(filePath, fakeCmdPath, 0755)
 	if err != nil {
-		fmt.Printf("%s%s\n", tags.Err, err.Error())
-	}
-	fakeCmdPath := filepath.Join(newDir, fakeCmd)
-	defer func() {
-		err := os.RemoveAll(newDir)
-		if err != nil {
-			fmt.Printf("%sОшибка при удалении %s: %s\n", tags.Log, fakeCmdPath, err.Error())
-		} else {
-			fmt.Printf("%sФайл %s успешно удален\n", tags.Log, fakeCmdPath)
-		}
-	}()
-	system.UpdatePath([]string{newDir})
-	err = system.CopyFile(filepath.Join("assets", fileName), fakeCmdPath, 0755)
-	if err != nil {
-		fmt.Printf("%sОшибка при копировании файла %s: %s\n", tags.Err, fileName, err.Error())
+		fmt.Printf("%sError when copying a file %s: %s\n", tags.Err, fileName, err.Error())
 		return
 	}
-	fmt.Printf("%sФайл %s успешно скопирован в %s\n", tags.Info, fileName, fakeCmdPath)
-	out, err := exec.Command("su").Output()
+	fmt.Printf("%sFile %s has been successfully copied to %s\n", tags.Info, fileName, fakeCmdPath)
+
+	out, err := exec.Command(fakeCmd).Output()
 	if err != nil {
-		fmt.Printf("%s%s\n", tags.Err, err.Error())
+		fmt.Printf("%sError while executing the %s command: %s\n", tags.Err, fakeCmd, err.Error())
 		return
 	}
 	fmt.Print(string(out))
+}
+
+func createDir(tempDirPath string) {
+	err := os.Mkdir(tempDirPath, 0755)
+	if err != nil {
+		fmt.Printf("%sError when creating a directory %s:%s\n", tags.Err, tempDirPath, err.Error())
+	}
+}
+
+func deleteDir(tempDirPath string) {
+	err := os.RemoveAll(tempDirPath)
+	if err != nil {
+		fmt.Printf("%sError when deleting a %s: %s\n", tags.Log, tempDirPath, err.Error())
+	} else {
+		fmt.Printf("%sFile %s successfully deleted\n", tags.Log, tempDirPath)
+	}
 }
