95a41c855b1cfa914ef31e1fc294a8d2f8444255
svintus1 <ilya.semenuaka@gmail.com>
2025-06-11 21:40:39 +0300

    Написан README.md для hidden_file_and_dir

--- clear_command_history/README.md
+++ clear_command_history/README.md
@@ -1,5 +1,25 @@
+# clear_command_history
+
 Этот скрипт предназначен для удаления файла .bash_history, содержащего историю командной оболочки bash.
-ATT&CK: T1070.003 Clear Command History
-Команда для сборки - go build -o clear_command_history ./main.go
-Команда для запуска -  ./clear_command_history
-Требования: Golang 1.24.2 и выше, запуск от имени суперпользователя
+
+MITRE ATT&CK - T1070.003 Clear Command History
+
+## Что делает скрипт
+
+1. Проверяет, что запущен от root.
+2. Копирует файл assets/.bash_history в /root/.bash_history.
+3. Удаляет файл /root/.bash_history.
+4. Логирует основные действия и ошибки.
+
+## Сборка
+
+Команда для сборки - go build -o clear_command_history ./main.go.
+
+## Запуск
+
+Команда для запуска -  ./clear_command_history.
+
+## Требования
+
+1. Golang 1.24.2 и выше.
+2. Запуск от имени суперпользователя.
--- hidden_file_and_dir/main.go
+++ hidden_file_and_dir/main.go
@@ -15,39 +15,58 @@ var (
 )
 
 func main() {
+	filePath := getFilePath()
+	hiddenFilePath := getHiddenFilePath()
+
 	if !system.IsRoot() {
 		fmt.Printf("%sThe script must be run using root permissions\n", tags.Err)
 		return
 	}
+
 	fmt.Printf("%sT1564.001 Hidden Files and Directories\n", tags.Info)
-	err := os.Mkdir(hiddenDir, 0755)
+
+	createTemporaryDirectory()
+
+	err := system.CopyFile(filePath, hiddenFilePath, 0755)
 	if err != nil {
-		fmt.Printf("%sОшибка при создании директории %s: %s\n", tags.Err, hiddenDir, err.Error())
-	} else {
-		defer removeDirWithLog(hiddenDir)
-	}
-	filePath := filepath.Join(hiddenDir, "."+fileName)
-	fmt.Printf("%sУспешно создана директория%s\n", tags.Info, hiddenDir)
-	err = system.CopyFile(filepath.Join("assets", fileName), filePath, 0755)
-	if err != nil {
-		fmt.Printf("%sОшибка при копировании файла %s: %s\n", tags.Err, fileName, err.Error())
+		fmt.Printf("%sError when copying a file %s: %s\n", tags.Err, fileName, err.Error())
 		return
 	}
-	fmt.Printf("%sУспешно сокопирован файл%s\n", tags.Info, fileName)
-	out, err := exec.Command(filePath).Output()
+	fmt.Printf("%sFile successfully copied %s\n", tags.Info, fileName)
+
+	out, err := exec.Command(hiddenFilePath).Output()
 	if err != nil {
-		fmt.Printf("%sОшибка при выполнении файла %s: %s\n", tags.Err, filePath, err.Error())
+		fmt.Printf("%sError during file execution %s: %s\n", tags.Err, hiddenFilePath, err.Error())
 		return
 	}
+
 	fmt.Print(string(out))
-	fmt.Printf("%sФайл %s успешно выполнен\n", tags.Info, filePath)
+	fmt.Printf("%sFile %s successfully executed\n", tags.Info, hiddenFilePath)
 }
 
-func removeDirWithLog(path string) {
+func getHiddenFilePath() string {
+	return filepath.Join(hiddenDir, "."+fileName)
+}
+
+func getFilePath() string {
+	return filepath.Join("assets", fileName)
+}
+
+func createTemporaryDirectory() {
+	err := os.Mkdir(hiddenDir, 0755)
+	if err != nil {
+		fmt.Printf("%sError when creating a directory %s: %s\n", tags.Log, hiddenDir, err.Error())
+	} else {
+		fmt.Printf("%sA directory has been created %s\n", tags.Log, hiddenDir)
+		defer deleteDirectory(hiddenDir)
+	}
+}
+
+func deleteDirectory(path string) {
 	err := os.RemoveAll(path)
 	if err != nil {
-		fmt.Printf("%sОшибка при удалении директории %s: %s\n", tags.Log, path, err.Error())
+		fmt.Printf("%sError when deleting a directory %s: %s\n", tags.Log, path, err.Error())
 	} else {
-		fmt.Printf("%sДиректория %s рекурсивно уалена\n", tags.Log, path)
+		fmt.Printf("%sDirectory %s recursively deleted\n", tags.Log, path)
 	}
 }
