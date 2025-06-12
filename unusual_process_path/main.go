bc0126be8c5e83e242025454b8b50329d56c1644
svintus1 <ilya.semenuaka@gmail.com>
2025-06-12 20:52:06 +0300

    Доработан unusual_process_path

--- unusual_process_path/main.go
+++ unusual_process_path/main.go
@@ -10,31 +10,45 @@ import (
 )
 
 var (
-	path     = "/var/log"
-	fileName = "myapp"
+	unusualPath = "/var/log"
+	fileName    = "myapp"
 )
 
 func main() {
+	filePath := filepath.Join("assets", fileName)
+	newFilePath := filepath.Join(unusualPath, fileName)
+
 	if !system.IsRoot() {
 		fmt.Printf("%sThe script must be run using root permissions\n", tags.Err)
 		return
 	}
+
 	fmt.Printf("%sProcesses in Unusual Paths\n", tags.Info)
-	err := system.CopyFile(filepath.Join("assets", fileName), filepath.Join(path, fileName), 0755)
+
+	err := system.CopyFile(filePath, newFilePath, 0755)
 	if err != nil {
 		fmt.Printf("%sError when copying a binary file\n", tags.Err)
 		return
 	}
-	absPath := filepath.Join(path, fileName)
-	defer func() {
-		os.Remove(absPath)
-		fmt.Printf("%sFile %s removed\n", tags.Log, absPath)
-	}()
-	cmd := exec.Command(absPath)
-	err = cmd.Run()
+	defer deleteFile(newFilePath)
+
+	err = launchExecFile(newFilePath)
 	if err != nil {
-		fmt.Printf("%sError when executing a binary file\n", tags.Err)
+		fmt.Printf("%sError when executing a binary file %s: %s\n", tags.Err, filePath, err.Error())
 		return
 	}
-	fmt.Printf("%sFile %s successfully launched in %s\n", tags.Info, fileName, absPath)
+	fmt.Printf("%sFile %s successfully launched\n", tags.Log, filePath)
+}
+
+func deleteFile(filePath string) {
+	os.Remove(filePath)
+	fmt.Printf("%sFile %s removed\n", tags.Log, filePath)
+}
+
+func launchExecFile(filePath string) error {
+	err := exec.Command(filePath).Run()
+	if err != nil {
+		return fmt.Errorf("error when executing file %s: %s", filePath, err.Error())
+	}
+	return nil
 }
